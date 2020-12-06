09bbb0f9c69a316de80eb4401965cfd6874cd2af
ts-dmitry <dmitry.tsylind@gmail.com>
2020-12-06 14:10:37 +0100

    Remove unnecessary icons in assets folder on the frontend. Change 404 page content and add it to router on the frontend.

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -5,6 +5,7 @@ import Home from "@/pages/Home";
 import WorkingTime from "@/pages/WorkingTime";
 import Settings from "@/pages/Settings";
 import About from "@/pages/About";
+import Page404 from "@/pages/Page404";
 
 const router = new VueRouter({
     mode: 'history',
@@ -13,7 +14,8 @@ const router = new VueRouter({
         {path: '/working-time', component: WorkingTime},
         {path: '/settings', component: Settings},
         {path: '/about', component: About},
-    ]
+        {path: "*", component: Page404}
+]
 });
 
 Vue.use(VueRouter)
--- frontend/src/pages/Page404.vue
+++ frontend/src/pages/Page404.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    This is page 404
+  <div class="v-sheet pt-5">
+    <v-row class="justify-center">
+      <h1 class="text-lg-h1">404</h1>
+    </v-row>
+    <v-row class="justify-center">
+      <h2 class="text-lg-h2">PAGE NOT FOUND</h2>
+    </v-row>
+
+    <v-divider class="ma-5"></v-divider>
+
+    <v-row class="justify-center">
+      <span>The page you are looking for might have been removed or is temporarily unavailable.</span>
+    </v-row>
   </div>
 </template>
 
