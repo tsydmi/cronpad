b810eb1da43f742ee5233fccf5ce6bb8a33d11f8
ts-dmitry <dmitry.tsylind@gmail.com>
2021-03-08 13:46:59 +0100

    Predefine 3 basic tags in mongo image. Allow registration in keycloak service. Add proxy container to docker-compose, remove all unnecessary ports and add images to each service.

---
+++ docker/mongo/init/02_create_basic_tags.js
@@ -0,0 +1,5 @@
+db.tag.insertMany([
+	{ "name": "Work", "description": "work", "color": "#81D4FA", "basic": true },
+	{ "name": "Meeting", "description": "meeting", "color": "#80CBC4", "basic": true },
+	{ "name": "Rest", "description": "rest", "color": "#FFCC80", "basic": true }
+]);
